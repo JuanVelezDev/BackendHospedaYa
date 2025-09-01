1c1ea0777df7c938a32bf8a0d1880eac09dd9059
JuanVelezDev <juanes.jevj@gmail.com>
2025-09-01 06:35:15 -0500

    Refactor service files to update database import paths from "../db.js" to "../database.js" for consistency across the project.

--- services/service.apartment.js
+++ services/service.apartment.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import db from "../db.js";
+import db from "../database.js";
 import upload from "../Middlewares/upload.js";
 import cloudinary from "../config/cloudinary.js";
 import streamifier from "streamifier";
